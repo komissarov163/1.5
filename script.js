e7cd443ac3b3f993b94f5d99d177c6e27b81c166
komissarov163 <104564270+komissarov163@users.noreply.github.com>
2022-08-24 18:57:13 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+if (document.documentElement.clientWidth < 420) {
+  var swiper = new Swiper(".mySwiper", {
+    slidesPerView: "auto",
+    spaceBetween: 16,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+}
+ 
+
+
+
+
+
+   
